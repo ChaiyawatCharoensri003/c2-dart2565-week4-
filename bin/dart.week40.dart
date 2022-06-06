21a5c38d00e7574a1adf525653f4c35eb3814e4b
wayusangchun <wayu.best.2549@gmail.com>
2022-06-06 12:36:13 +0700

    update

---
+++ bin/dart.week40.dart
@@ -0,0 +1,17 @@
+void main(){
+  var a = "Hello";
+  a ="H1";
+  a = "1000";
+  dynamic b = "Welcome to thailand"; //String type
+  b = "very good";
+  b = 100;
+  
+  String c = "Thailand";
+  dynamic d =[1,2,3,4];//List type
+  
+  //Prunt is Display to monitor
+  print(a);
+  print(b);
+  print(c);
+  print(d(1));
+}
